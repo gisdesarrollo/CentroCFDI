08009a3ccc23b92e56ebd8ea5c9ba6cdd850a82d
kmaur <kmaur@LAPTOP-NHA7UV05>
2022-11-10 13:09:56 -0600

    Se suben cambios de graficas y reportes

--- API/Models/Control/HomeModel.cs
+++ API/Models/Control/HomeModel.cs
@@ -1,18 +1,88 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace API.Models.Control
 {
+
     public class HomeModel
     {
+
+        [DisplayName("Clientes Totales")]
         public int Clientes { get; set; }
 
         [DisplayName("CFDIs Emitidos")]
         public int CfdiEmitidos { get; set; }
 
+        [DisplayName("Suma Total CFDIs Emitidos")]
+        public int TotalCfdiEmitidos { get; set; }
+
         [DisplayName("Complementos de Pago Emitidos")]
         public int ComplementosPagoEmitidos { get; set; }
 
+        [DisplayName("Complementos de Carta Porte Pendientes")]
+        public int ComplementoCartaPortePendientes { get; set; }
+
         [DisplayName("Complementos de Pago Pendientes")]
         public int ComplementosPagoPendientes { get; set; }
+
+        [DisplayName("Complementos Cfdi Pendientes")]
+        public int ComprobanteCfdiPendientes { get; set; }
+
+        [DisplayName("CTotal")]
+        public int Total { get; set; }
+
+        [DisplayName("Monto CFDI Total")]
+        public int MontoCFDI { get; set; }
+
+
+        //Monto de Meses anteriores asta la fecha actual
+
+        public int Mes1total { get; set; }
+
+
+        [DisplayName("Monto CFDI Total del Mes Segundo")]
+        public int Mes2total { get; set; }
+
+        [DisplayName("Monto CFDI Total del Mes Tercero ")]
+        public int Mes3total { get; set; }
+
+        [DisplayName("Monto CFDI Total del Mes Cuarto")]
+        public int Mes4total { get; set; }
+
+        [DisplayName("Monto CFDI Total del Mes Quinto ")]
+        public int Mes5total { get; set; }
+
+        [DisplayName("Monto CFDI Total del Mes Sexto")]
+        public int Mes6total { get; set; }
+
+
+        
+        //Nombres de Meses Anteriores asta la fecha actual
+        public string Mes1 { get; set; }
+
+        public string Mes2 { get; set; }
+
+        public string Mes3 { get; set; }
+
+        public string Mes4 { get; set; }
+
+        public string Mes5 { get; set; }
+
+        public string Mes6 { get; set; }
+
+
+
+
     }
+
+
+   
+
+    
+
+
+
+
+
 }
--- Aplicacion/LogicaPrincipal/Control/PopularHome.cs
+++ Aplicacion/LogicaPrincipal/Control/PopularHome.cs
@@ -27,15 +27,132 @@ namespace Aplicacion.LogicaPrincipal.Control
         #endregion
 
         public void Popular(ref HomeModel homeModel)
+
+
         {
-            var fechaInicial = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            
+
+           //Mes actual desde el primer dia hasta el dia actual 
+
+            var fechaInicial = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+
             var fechaFinal = DateTime.Now;
 
-            homeModel.CfdiEmitidos = _db.FacturasEmitidas.Count(fe => fe.EmisorId == _sucursalId && fe.Fecha >= fechaInicial && fe.Fecha <= fechaFinal);
+           
+            //Fechas de Acorde a los ingresos por cada mes  de los 6 meses hasta el mes actual
+
+            //Mes Segundo
+            var fechaInicial2 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-1).AddDays(0);
+
+            var fechaFinal2 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-0).AddDays(-1);
+
+            //Mes Tercero
+            var fechaInicial3 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-2).AddDays(0);
+
+            var fechaFinal3 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-1).AddDays(-1);
+
+            //Mes Cuarto
+            var fechaInicial4 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-3).AddDays(0);
+
+            var fechaFinal4 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-2).AddDays(-1);
+
+            //Mes Quinto
+            var fechaInicial5 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-4).AddDays(0);
+
+            var fechaFinal5 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-3).AddDays(-1);
+
+            //Mes Sexto
+            var fechaInicial6 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-5).AddDays(0);
+
+            var fechaFinal6 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-4).AddDays(-1);
+
+
+
+            //Nombres de los Meses anteriores hasta el actual
+
+            string mes1 = DateTime.Now.ToString("MMM");
+
+            string mes2 = DateTime.Now.AddMonths(-1).ToString("MMM");
+
+            string mes3 = DateTime.Now.AddMonths(-2).ToString("MMM");
+
+            string mes4 = DateTime.Now.AddMonths(-3).ToString("MMM");
+
+            string mes5 = DateTime.Now.AddMonths(-4).ToString("MMM");
+
+            string mes6 = DateTime.Now.AddMonths(-5).ToString("MMM");
+
+
+            //Asignacion de los meses 
+
+            homeModel.Mes1 = mes1;
+
+            homeModel.Mes2 = mes2;
+
+            homeModel.Mes3 = mes3;
+
+            homeModel.Mes4 = mes4;
+
+            homeModel.Mes5 = mes5;
+
+            homeModel.Mes6 = mes6;
+
+
+
+            //var fechaIniant = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(0).AddDays(0);
+
+            // var fechaFiant = DateTime.Now;
+
+
+            //homeModel.CfdiEmitidos = _db.FacturasEmitidas.Count(fe => fe.EmisorId == _sucursalId && fe.Fecha >= fechaIniant && fe.Fecha <= fechaFiant);
+
+            //Clientes Totales
             homeModel.Clientes = _db.Clientes.Count(c => c.SucursalId == _sucursalId);
-            homeModel.ComplementosPagoEmitidos = _db.ComplementosPago.Count(fe => fe.SucursalId == _sucursalId && fe.Generado && fe.FechaDocumento >= fechaInicial && fe.FechaDocumento <= fechaFinal);
-            homeModel.ComplementosPagoPendientes = _db.ComplementosPago.Count(fe => fe.SucursalId == _sucursalId && !fe.Generado && fe.FechaDocumento >= fechaInicial && fe.FechaDocumento <= fechaFinal);
+
+            //Facturas emitidas del mes presente
+            homeModel.CfdiEmitidos = _db.FacturasEmitidas.Count(fe => fe.EmisorId == _sucursalId && fe.Fecha >= fechaInicial && fe.Fecha <= fechaFinal);
+
+
+            //Complemento Pago Pendientes 
+            var ComplementoPagoPen = homeModel.ComplementosPagoPendientes = _db.ComplementosPago.Count(fe => fe.SucursalId == _sucursalId && !fe.Generado && fe.FechaDocumento >= fechaInicial && fe.FechaDocumento <= fechaFinal);
+
+
+            //Complemento Carta Porte Pendientes
+            var ComplementoCartaPen = homeModel.ComplementoCartaPortePendientes = _db.ComplementoCartaPortes.Count(fe => fe.SucursalId == _sucursalId && !fe.Generado && fe.FechaDocumento >= fechaInicial && fe.FechaDocumento <= fechaFinal);
+
+
+            //Complemento Cfdi Pendientes
+            var ComplementoCfdiPen = homeModel.ComprobanteCfdiPendientes = _db.ComprobantesCfdi.Count(fe => fe.SucursalId == _sucursalId && !fe.Generado && fe.FechaDocumento >= fechaInicial && fe.FechaDocumento <= fechaFinal);
+
+            //suma de todos los comprobantes CFDI pendientes
+            var sumaCFDi = homeModel.Total = ComplementoCartaPen + ComplementoPagoPen + ComplementoCfdiPen;
+
+            //Monto de total  de ingreso del mes presente  correspondiente a Facturas Emitidas
+            homeModel.MontoCFDI = (int)_db.FacturasEmitidas.Where(fe => fe.TipoComprobante==API.Enums.c_TipoDeComprobante.I && fe.Fecha >= fechaInicial &&  fe.Fecha <= fechaFinal ).Select(fe => fe.Total).DefaultIfEmpty(0).Sum();
+
+
+            //Rango de fechas para obtener los ingresos por cada mes 
+            
+
+            homeModel.Mes2total = (int)_db.FacturasEmitidas.Where(fe => fe.TipoComprobante == API.Enums.c_TipoDeComprobante.I && fe.Fecha >= fechaInicial2 && fe.Fecha <= fechaFinal2).Select(fe => fe.Total).DefaultIfEmpty(0).Sum(); ;
+
+            homeModel.Mes3total = (int)_db.FacturasEmitidas.Where(fe => fe.TipoComprobante == API.Enums.c_TipoDeComprobante.I && fe.Fecha >= fechaInicial3 && fe.Fecha <= fechaFinal3).Select(fe => fe.Total).DefaultIfEmpty(0).Sum(); ;
+
+            homeModel.Mes4total = (int)_db.FacturasEmitidas.Where(fe => fe.TipoComprobante == API.Enums.c_TipoDeComprobante.I && fe.Fecha >= fechaInicial4 && fe.Fecha <= fechaFinal4).Select(fe => fe.Total).DefaultIfEmpty(0).Sum(); ;
+
+            homeModel.Mes5total = (int)_db.FacturasEmitidas.Where(fe => fe.TipoComprobante == API.Enums.c_TipoDeComprobante.I && fe.Fecha >= fechaInicial5 && fe.Fecha <= fechaFinal5).Select(fe => fe.Total).DefaultIfEmpty(0).Sum(); ;
+
+            homeModel.Mes6total = (int)_db.FacturasEmitidas.Where(fe => fe.TipoComprobante == API.Enums.c_TipoDeComprobante.I && fe.Fecha >= fechaInicial6 && fe.Fecha <= fechaFinal6).Select(fe => fe.Total).DefaultIfEmpty(0).Sum(); ;
+
+
+
+
+
+
+
         }
-        
+
+
+
     }
 }
